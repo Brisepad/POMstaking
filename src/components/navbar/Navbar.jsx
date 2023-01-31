104f60a25d7cfb685865695e1163179174bb0ebe
OkekeChristian24 <christian.okeke24@gmail.com>
2023-01-31 11:45:47 -0800

    feat: modify content

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { useWeb3React } from '@web3-react/core';
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import { Link } from 'react-router-dom';
-import useAuth from '../../hooks/useAuth';
-import logo from '../../logo.svg';
-import truncateWalletAddress from '../../utils/truncateWalletAddress';
-import AccountModal from '../wallet/AccountModal';
-import { connectorLocalStorageKey } from '../wallet/config';
-import WalletModal from '../wallet/WalletModal';
-import './navbar.css';
+import React, { useState, useEffect } from "react";
+import { useWeb3React } from "@web3-react/core";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { Link, NavLink } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import logo from "../../logo.svg";
+import truncateWalletAddress from "../../utils/truncateWalletAddress";
+import AccountModal from "../wallet/AccountModal";
+import { connectorLocalStorageKey } from "../wallet/config";
+import WalletModal from "../wallet/WalletModal";
+import "./navbar.css";
 
 const Navbar = () => {
   const { login, logout } = useAuth();
-  const { active, account, library, connector, activate, deactivate } = useWeb3React()
-
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
 
   const [toggleMenu, setToggleMenu] = useState(false);
   const [openWalletModal, setOpenWalletModal] = useState(false);
@@ -50,65 +50,135 @@ const Navbar = () => {
           window.initWeb3 = library;
           // localStorage.setItem('isWalletConnected', true)
         } catch (ex) {
-          console.log(ex)
+          console.log(ex);
         }
       }
-    }
+    };
     connectWalletOnPageLoad();
   }, []);
 
-
   return (
     <>
-    <div className="gpt3__navbar">
-      <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo">
-          <Link to="/">
-            <img style={{ marginBottom: -5 }} src='/images/briselogo/brise-vertical.png' alt="BrisePad" />
-          </Link>
-          {/* <img style={{ marginBottom: -5 }} src={logo} alt="PoolApp" /> */}
-          {/* <h2 style={{color: '#ffffff'}}>BrisePad</h2> */}
-        </div>
-        <div className="gpt3__navbar-links_container">
-          <p><Link to="/">Home</Link></p>
-          <p><Link to="/activepools">Pools</Link></p>
-          <p><Link to="/inactivepools">Finished</Link></p>
-          
-        </div>
-      </div>
-      <div className="gpt3__navbar-sign">
-        {/* <p>Get Loan</p> */}
-        <button onClick={showWalletModal} type="button">{active ? truncateWalletAddress(account): "Connect"}</button>
-      </div>
-      <div className="gpt3__navbar-menu">
-        {toggleMenu
-          ? <RiCloseLine color="#fff" size={27} onClick={closeToggleMenu} />
-          : <RiMenu3Line color="#fff" size={27} onClick={showToggleMenu} />}
-        {toggleMenu && (
-        <div className="gpt3__navbar-menu_container scale-up-center">
-          <div className="gpt3__navbar-menu_container-links">
-          <p><Link onClick={closeToggleMenu} to="/">Home</Link></p>
-          <p><Link onClick={closeToggleMenu} to="/activepools">Pools</Link></p>
-          <p><Link onClick={closeToggleMenu} to="/inactivepools">Finished</Link></p>
+      <div className="gpt3__navbar">
+        <div className="gpt3__navbar-links">
+          <div className="gpt3__navbar-links_logo">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "activeLink" : undefined
+              }
+            >
+              <h3 id="text_logo">POM PROTOCOL</h3>
+              {/* <img style={{ marginBottom: -5 }} src='/images/briselogo/brise-vertical.png' alt="BrisePad" /> */}
+            </NavLink>
           </div>
-          <div className="gpt3__navbar-menu_container-links-sign">
-            {/* <p>Launch Loan App</p> */}
-            <button onClick={showWalletModal} type="button">{active ? truncateWalletAddress(account): "Connect"}</button>
+          <div className="gpt3__navbar-links_container">
+            <p>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "activeLink" : undefined
+                }
+              >
+                Home
+              </NavLink>
+            </p>
+            <p>
+              <NavLink
+                to="/activepools"
+                className={({ isActive }) =>
+                  isActive ? "activeLink" : undefined
+                }
+              >
+                Pools
+              </NavLink>
+            </p>
+            <p>
+              <NavLink
+                to="/inactivepools"
+                className={({ isActive }) =>
+                  isActive ? "activeLink" : undefined
+                }
+              >
+                Finished
+              </NavLink>
+            </p>
           </div>
         </div>
-        )}
+        <div className="gpt3__navbar-sign">
+          {/* <p>Get Loan</p> */}
+          <button onClick={showWalletModal} type="button">
+            {active ? truncateWalletAddress(account) : "Connect"}
+          </button>
+        </div>
+        <div className="gpt3__navbar-menu">
+          {toggleMenu ? (
+            <RiCloseLine color="#fff" size={27} onClick={closeToggleMenu} />
+          ) : (
+            <RiMenu3Line color="#fff" size={27} onClick={showToggleMenu} />
+          )}
+          {toggleMenu && (
+            <div className="gpt3__navbar-menu_container scale-up-center">
+              <div className="gpt3__navbar-menu_container-links">
+                <p>
+                  <NavLink
+                    onClick={closeToggleMenu}
+                    to="/"
+                    className={({ isActive }) =>
+                      isActive ? "activeLink" : undefined
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </p>
+                <p>
+                  <NavLink
+                    onClick={closeToggleMenu}
+                    to="/activepools"
+                    className={({ isActive }) =>
+                      isActive ? "activeLink" : undefined
+                    }
+                  >
+                    Pools
+                  </NavLink>
+                </p>
+                <p>
+                  <NavLink
+                    onClick={closeToggleMenu}
+                    to="/inactivepools"
+                    className={({ isActive }) =>
+                      isActive ? "activeLink" : undefined
+                    }
+                  >
+                    Finished
+                  </NavLink>
+                </p>
+              </div>
+              <div className="gpt3__navbar-menu_container-links-sign">
+                {/* <p>Launch Loan App</p> */}
+                <button onClick={showWalletModal} type="button">
+                  {active ? truncateWalletAddress(account) : "Connect"}
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-    {
-      openWalletModal && ( 
-        active ?
-        <AccountModal account={account} logout={accountLogout} onDismiss={closeWalletModal} />
-        :
-        <WalletModal login={login} logout={logout} onDismiss={closeWalletModal} />
-      )
-    }
+      {openWalletModal &&
+        (active ? (
+          <AccountModal
+            account={account}
+            logout={accountLogout}
+            onDismiss={closeWalletModal}
+          />
+        ) : (
+          <WalletModal
+            login={login}
+            logout={logout}
+            onDismiss={closeWalletModal}
+          />
+        ))}
     </>
-
   );
 };
 
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import gpt3Logo from '../../logo.svg';
-import './footer.css';
+import React from "react";
+import gpt3Logo from "../../logo.svg";
+import "./footer.css";
 
 const Footer = () => (
-  <div id="footer" style={{padding: '5px'}} className="gpt3__footer section__padding fixed__bottom">
+  <div
+    id="footer"
+    style={{ padding: "5px" }}
+    className="gpt3__footer section__padding fixed__bottom"
+  >
     {/* <div className="gpt3__footer-heading">
       <h1 className="gradient__text">Do you want to step in to the future before others</h1>
     </div>
@@ -15,7 +19,7 @@ const Footer = () => (
 
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
-      <p>@2022 All rights reserved.  BrisePad</p>
+        <p>@2022 All rights reserved. POM Protocol</p>
       </div>
       {/* <div className="gpt3__footer-links_div">
         <p>Twitter</p>
@@ -23,7 +27,6 @@ const Footer = () => (
         <p>Telegram</p>
         <p>Discord</p>
       </div> */}
-    
     </div>
 
     {/* <div className="gpt3__footer-copyright">
